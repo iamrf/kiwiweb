3bafab361060aba5e51fbeab27c6e85e1f0dcdc2
rf <rf.fa17@gmail.com>
2019-12-17 22:43:23 +0330

    Blog Post, Tag, Category defined in models.py and admin.py, also blog app added to INSTALLED_APPS in base settings

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'modified',)
+    list_filter = ('created', 'modified')
+    search_fields = ('title', 'created', 'modified',)
+    date_hierarchy = 'created'
+    ordering = ['title', '-created', '-modified',]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'modified',)
+    list_filter = ('created', 'modified')
+    search_fields = ('title', 'created', 'modified',)
+    date_hierarchy = 'created'
+    ordering = ['title', '-created', '-modified',]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', 'modified', 'status')
+    list_filter = ('created', 'modified', 'date', 'author')
+    search_fields = ('title', 'content','author', 'date', 'created', 'modified', 'status',)
+    date_hierarchy = 'date'
+    ordering = ['-date', 'title', '-created', '-modified',]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+class Category(TimeStampedModel):
+    title = models.CharField(max_length=250, verbose_name="Title", unique=True)
+    slug = models.SlugField(max_length=250, verbose_name="Slug", unique=True)
+
+    class Meta:
+        ordering = ('title', '-created', '-modified')
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(TimeStampedModel):
+    title = models.CharField(max_length=250, verbose_name="Title", unique=True)
+    slug = models.SlugField(max_length=250, verbose_name="Slug", unique=True)
+
+    class Meta:
+        ordering = ('title', '-created', '-modified')
+
+    def __str__(self):
+        return self.title
+
+
+# Define a manager for published posts
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='publish')
+
+
+class Post(TimeStampedModel):
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('publish', 'Publish'),
+    )
+
+    title = models.CharField(max_length=250, verbose_name="Title")
+    slug = models.SlugField(max_length=250, verbose_name="Slug", unique=True)
+    content = models.TextField(verbose_name="Content")
+    categories = models.ManyToManyField(Category, blank=True, verbose_name="Categories")
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name="Tags")
+    date = models.DateTimeField(default=timezone.now, verbose_name="Publish date")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Author")
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="draft")
+
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # The published posts manager
+
+    class Meta:
+        ordering: ('-date', 'title', 'author',)
+
+    def __str__(self):
+        return self.title
