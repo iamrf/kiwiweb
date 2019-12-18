07496ac5bc8525771c3d1b6cab38a63eaee93892
rf <rf.fa17@gmail.com>
2019-12-18 05:52:48 +0330

    Comments added to blog app

--- blog/admin.py
+++ blog/admin.py
@@ -21,14 +21,23 @@ class TagAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'date', 'modified', 'status')
-    list_filter = ('created', 'modified', 'date', 'author')
-    search_fields = ('title', 'content','author', 'date', 'created', 'modified', 'status',)
+    list_display = ('title', 'author', 'date', 'modified', 'publish')
+    list_filter = ('date', 'author', 'publish',)
+    search_fields = ('title', 'content','author', 'date', 'created', 'modified', 'publish',)
     date_hierarchy = 'date'
     ordering = ['-date', 'title', '-created', '-modified',]
     prepopulated_fields = {'slug': ('title',)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created', 'publish',)
+    list_filter = ('created', 'publish',)
+    search_fields = ('content','name', 'email', 'created', 'publish',)
+    date_hierarchy = 'created'
+    ordering = ['-created', 'name', 'email',]
+
+
 admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.Tag, TagAdmin)
 admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.utils import timezone
 from core.models import TimeStampedModel
 
 
+# Define a manager for published posts
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(publish=True)
+
+
 class Category(TimeStampedModel):
     title = models.CharField(max_length=250, verbose_name="Title", unique=True)
     slug = models.SlugField(max_length=250, verbose_name="Slug", unique=True)
@@ -26,27 +32,15 @@ class Tag(TimeStampedModel):
         return self.title
 
 
-# Define a manager for published posts
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(status='publish')
-
-
 class Post(TimeStampedModel):
-
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('publish', 'Publish'),
-    )
-
     title = models.CharField(max_length=250, verbose_name="Title")
     slug = models.SlugField(max_length=250, verbose_name="Slug", unique=True)
     content = models.TextField(verbose_name="Content")
-    categories = models.ManyToManyField(Category, blank=True, verbose_name="Categories")
-    tags = models.ManyToManyField(Tag, blank=True, verbose_name="Tags")
+    categories = models.ManyToManyField(Category, related_name="categories", blank=True, verbose_name="Categories")
+    tags = models.ManyToManyField(Tag, related_name="tags", blank=True, verbose_name="Tags")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author", verbose_name="Author")
     date = models.DateTimeField(default=timezone.now, verbose_name="Publish date")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Author")
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="draft")
+    publish = models.BooleanField(verbose_name="Published", default=False)
 
     objects = models.Manager()  # The default manager
     published = PublishedManager()  # The published posts manager
@@ -56,3 +50,20 @@ class Post(TimeStampedModel):
 
     def __str__(self):
         return self.title
+
+
+class Comment(TimeStampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="Post", verbose_name="Post")
+    name = models.CharField(max_length=100, verbose_name="Name")
+    email = models.EmailField(verbose_name="Email")
+    content = models.TextField(verbose_name="Content")
+    publish = models.BooleanField(verbose_name="Published", default=True)
+    
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering: ('-created', 'name', 'email',)
+
+    def __str__(self):
+        return self.email
