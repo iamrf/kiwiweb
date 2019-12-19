15bf576b5361cd75ce23f382d3a2bc05b276e0d0
rf <rf.fa17@gmail.com>
2019-12-19 15:33:56 +0330

    related_names fixed in blog.models

--- blog/models.py
+++ blog/models.py
@@ -42,9 +42,9 @@ class Post(TimeStampedModel):
     title = models.CharField(max_length=250, verbose_name="Title")
     slug = models.SlugField(max_length=250, verbose_name="Slug", unique=True)
     content = models.TextField(verbose_name="Content")
-    categories = models.ManyToManyField(Category, related_name="categories", blank=True, verbose_name="Categories")
-    tags = models.ManyToManyField(Tag, related_name="tags", blank=True, verbose_name="Tags")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author", verbose_name="Author")
+    categories = models.ManyToManyField(Category, related_name="posts", blank=True, verbose_name="Categories")
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True, verbose_name="Tags")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", verbose_name="Author")
     date = models.DateTimeField(default=timezone.now, verbose_name="Publish date")
     publish = models.BooleanField(verbose_name="Publish", default=False)
 
@@ -63,11 +63,11 @@ class Post(TimeStampedModel):
 
 
 class Comment(TimeStampedModel):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="Post", verbose_name="Post")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", verbose_name="Post")
     name = models.CharField(max_length=100, verbose_name="Name")
     email = models.EmailField(verbose_name="Email")
     content = models.TextField(verbose_name="Content")
-    publish = models.BooleanField(verbose_name="Published", default=True)
+    publish = models.BooleanField(verbose_name="Publish", default=True)
     
     objects = models.Manager()
     published = PublishedManager()
