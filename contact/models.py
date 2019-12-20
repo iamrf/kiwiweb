12fad0212e5fa3ebf5a87866ed60d1fe06448ea4
rf <rf.fa17@gmail.com>
2019-12-20 17:08:59 +0330

    Contact models defined

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+class Contact(TimeStampedModel):
+    name = models.CharField(max_length=200, verbose_name="Name")
+    email = models.EmailField(verbose_name="Email")
+    tel = models.CharField(max_length=15, blank=True, verbose_name="Tel")
+    subject = models.CharField(max_length=250, verbose_name="Subject")
+    content = models.TextField(verbose_name="Content")
+
+    def __str__(self):
+        return self.subject
