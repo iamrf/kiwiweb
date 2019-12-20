97d63906349261695942f9a73bc9e5eb93b60493
rf <rf.fa17@gmail.com>
2019-12-20 18:05:36 +0330

    Newsletter view defined

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from .forms import Newsletter
+from . import models
 
-# Create your views here.
+
+def get_newsletter(request):
+    if request.method == 'POST':
+        form = Newsletter(request.POST)
+
+        if form.is_valid():
+            new_form = models.Newsletter.objects.create(
+                name = form.cleaned_data['name'],
+                email = form.cleaned_data['email'],
+            )
+
+        return HttpResponseRedirect(reverse('newsletter:success'))
+    
+    else:
+        form = Newsletter()
+
+    context = {'form': form}
+    return render(request, 'newsletter/form_page.html', context)
+
+
+def success_newsletter(request):
+    return render(request, 'contact/success.html')
