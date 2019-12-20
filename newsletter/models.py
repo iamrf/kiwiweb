84e12d8b6717f882bb6dcc9df6ee6e7c94cc396b
rf <rf.fa17@gmail.com>
2019-12-20 17:23:08 +0330

    Newsletter model was created

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+class Newsletter(TimeStampedModel):
+    name = models.CharField(max_length=250, verbose_name="Name", blank=True)
+    email = models.EmailField(verbose_name="Email", unique=True)
+
+    def __str__(self):
+        return self.email
