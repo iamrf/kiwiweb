b3eb35cd38e31f5b49ccc39ac0f3f840fa5f0269
rf <rf.fa17@gmail.com>
2019-12-20 21:17:57 +0330

    Total except condition added to view and template

--- contact/views.py
+++ contact/views.py
@@ -6,25 +6,35 @@ from . import models
 
 
 def get_contact(request):
+    error_msg = False
+
     if request.method == "POST":
         form = Contact(request.POST)
 
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            new_form = models.Contact.objects.create(
-                name=form.cleaned_data['name'],
-                email=form.cleaned_data['email'],
-                tel=form.cleaned_data['tel'],
-                subject=form.cleaned_data['subject'],
-                content=form.cleaned_data['content'],
-                )
+            try:
+                new_form = models.Contact.objects.create(
+                    name=form.cleaned_data['name'],
+                    email=form.cleaned_data['email'],
+                    tel=form.cleaned_data['tel'],
+                    subject=form.cleaned_data['subject'],
+                    content=form.cleaned_data['content'],
+                    )
             
-            # redirect to a new URL:
-            return HttpResponseRedirect(reverse('contact:success'))
+            except:
+                error_msg = 'متاسفانه مشکلی پیش آمده، لطفا دوباره تلاش کنید'
+
+            else:
+                # redirect to a new URL:
+                return HttpResponseRedirect(reverse('contact:success'))
     else:
         form = Contact()
 
-    context = {'form': form,}
+    context = {
+        'form': form,
+        'error': error_msg,
+    }
     return render(request, 'contact/form_page.html', context)
 
 
