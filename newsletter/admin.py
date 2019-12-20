bbf3c096259d2b9c9f6c05778387dbfbe5d0ecb4
rf <rf.fa17@gmail.com>
2019-12-20 18:06:59 +0330

    Newsletter admin page designed

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'created', 'modified')
+    list_filter = ('created', 'modified')
+    search_fields = ('email', 'name', 'created')
+    date_hierarchy = 'created'
+    ordering = ('-created', 'email', 'name', '-modified')
+
+
+admin.site.register(models.Newsletter, NewsletterAdmin)
