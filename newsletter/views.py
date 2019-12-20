30e223427192aec417ea206a452f89fbdae34a89
rf <rf.fa17@gmail.com>
2019-12-20 18:36:18 +0330

    Newsletter --> IntegrityError handled in views for unique email submits error

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
+from django.db import IntegrityError
 from .forms import Newsletter
 from . import models
 
 
 def get_newsletter(request):
+    error_msg = False
+
     if request.method == 'POST':
         form = Newsletter(request.POST)
 
         if form.is_valid():
-            new_form = models.Newsletter.objects.create(
-                name = form.cleaned_data['name'],
-                email = form.cleaned_data['email'],
-            )
+            try:
+                new_form = models.Newsletter.objects.create(
+                    name = form.cleaned_data['name'],
+                    email = form.cleaned_data['email'],
+                )
 
-        return HttpResponseRedirect(reverse('newsletter:success'))
-    
+            except IntegrityError:
+                error_msg = 'این ایمیل قبلا در سیستم ثبت شده'
+
+            else:
+                return HttpResponseRedirect(reverse('newsletter:success'))
+
     else:
         form = Newsletter()
 
-    context = {'form': form}
+    context = {
+        'form': form,
+        'error': error_msg,
+        }
     return render(request, 'newsletter/form_page.html', context)
 
 
