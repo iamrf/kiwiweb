67cc76b80807b741525658984a6132951ec239d9
rf <rf.fa17@gmail.com>
2019-12-22 00:50:16 +0330

    Portfolio admin settings configured

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'modified',)
+    list_filter = ('created', 'modified')
+    search_fields = ('title', 'created', 'modified',)
+    date_hierarchy = 'created'
+    ordering = ['title', '-created', '-modified',]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'modified',)
+    list_filter = ('created', 'modified')
+    search_fields = ('title', 'created', 'modified',)
+    date_hierarchy = 'created'
+    ordering = ['title', '-created', '-modified',]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'modified', 'publish')
+    list_filter = ('created', 'publish',)
+    search_fields = ('title', 'desc','url', 'created', 'modified', 'publish',)
+    date_hierarchy = 'created'
+    ordering = ['slug', '-created', '-modified',]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.Portfolio, PortfolioAdmin)
