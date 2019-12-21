f5d1df1712643bfa8baccee58a10849cdd616ac5
rf <rf.fa17@gmail.com>
2019-12-21 18:05:42 +0330

    sort field added to admin module

--- services/admin.py
+++ services/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from . import models
 
 class WebsiteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'template', 'domain', 'host', 'price', 'created', 'publish')
+    list_display = ('title', 'template', 'domain', 'host', 'price', 'created', 'publish', 'sort')
     list_filter = ('created', 'domain', 'template', 'price')
     search_fields = ('title', 'template', 'domain', 'host', 'support', 'created', 'desc', 'price')
     date_hierarchy = 'created'
-    ordering = ['title', '-created', '-modified',]
+    ordering = ['sort', 'title', '-created', '-modified',]
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(models.website, WebsiteAdmin)
