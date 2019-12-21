a0022088c1a36533abe4eae7e56a5d0292c934c7
rf <rf.fa17@gmail.com>
2019-12-21 17:14:34 +0330

    website model created in services.models

--- services/models.py
+++ services/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+# Define a manager for published posts
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(publish=True)
+
+
+class website(TimeStampedModel):
+    title = models.CharField(max_length=200, verbose_name='Title')
+    slug = models.SlugField(max_length=200, verbose_name='Slug')
+    template = models.CharField(max_length=200, verbose_name="Template")
+    domain = models.CharField(max_length=200, verbose_name="Domain")
+    host = models.CharField(max_length=200, verbose_name="Host")
+    support = models.CharField(max_length=200, verbose_name="Support", blank=True)
+    op_1 = models.CharField(max_length=200, verbose_name="Option 1", blank=True)
+    op_2 = models.CharField(max_length=200, verbose_name="Option 2", blank=True)
+    op_3 = models.CharField(max_length=200, verbose_name="Option 3", blank=True)
+    op_4 = models.CharField(max_length=200, verbose_name="Option 4", blank=True)
+    op_5 = models.CharField(max_length=200, verbose_name="Option 5", blank=True)
+    op_6 = models.CharField(max_length=200, verbose_name="Option 6", blank=True)
+    op_7 = models.CharField(max_length=200, verbose_name="Option 7", blank=True)
+    op_8 = models.CharField(max_length=200, verbose_name="Option 8", blank=True)
+    op_9 = models.CharField(max_length=200, verbose_name="Option 9", blank=True)
+    desc = models.TextField(verbose_name="Description", blank=True)
+    price = models.CharField(max_length=50, verbose_name="Price")
+    publish = models.BooleanField(verbose_name="Publish", default=True)
+
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # The published posts manager
+
+    def __str__(self):
+        return self.title
