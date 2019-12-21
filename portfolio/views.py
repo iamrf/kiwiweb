03d5f4ce8c0ea4553d5b2a0da88b2b6d57c2ddef
rf <rf.fa17@gmail.com>
2019-12-22 00:50:45 +0330

    Portfolio views defined

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
-# Create your views here.
+
+def home(request):
+    portfolio = models.Portfolio.published.all()
+    context = {'portfolio': portfolio,}
+    return render(request, 'portfolio/home.html', context)
+
+
+def item(request, slug):
+    item = get_object_or_404(models.Portfolio, slug=slug)
+    context = {'item': item,}
+    return render(request, 'portfolio/item.html', context)
