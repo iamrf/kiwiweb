5112bb472eae001f82ba32ebf35312b589c4ffde
rf <rf.fa17@gmail.com>
2019-12-21 18:06:43 +0330

    services.views defined for home and website plans page

--- services/views.py
+++ services/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
-# Create your views here.
+
+def home(request):
+    web_plans = models.website.published.order_by('sort')
+    context = {'web_plans': web_plans,}
+    return render(request, 'services/home.html', context)
+
+def website(request, slug):
+    plan = get_object_or_404(models.website, slug=slug)
+    context = {'plan': plan,}
+    return render(request, 'services/web_plan.html', context)
